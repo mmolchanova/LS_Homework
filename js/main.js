4fd1e5cbe3a7f7745ec38f549cca2196ea2718dc
Mariya Molchanova <molchanova.m2903@gmail.com>
2017-11-12 23:52:08 +0700

    add section 'burgers' with adaptive + fullscreen menu

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ let burgerBtn = document.querySelector('.header__burger-btn')
 let burgerBtnLines = document.querySelector('.burger-btn__lines')
 let burgerBtnClose = document.querySelector('.burger-btn__close-lines')
 let fullscreenNav = document.querySelector('.fullscreen-nav')
+// console.log(burgerBtn)
+// console.log(burgerBtnLines)
+// console.log(burgerBtnClose)
+// console.log(fullscreenNav)
 
 burgerBtn.addEventListener('click', function showMenu () {
   burgerBtnLines.classList.toggle('visually-hidden')
   burgerBtnClose.classList.toggle('visually-hidden')
+
+// console.log(burgerBtnLines)
+// console.log(burgerBtnClose)
 
   let menuDisplay = getComputedStyle(fullscreenNav).display
   if (menuDisplay === 'none') {

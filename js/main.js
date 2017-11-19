0c758e7d04f6664e9dbca56a81b6afbd999ac0e0
Mariya Molchanova <molchanova.m2903@gmail.com>
2017-11-19 20:45:53 +0700

    slider for section 'burger'

--- js/main.js
+++ js/main.js
@@ -1,85 +1,159 @@
 // Для полноэкранного меню
-let burgerBtn = document.querySelector('.header__burger-btn')
-let burgerBtnBlock = document.querySelector('.burger-block')
-let burgerBtnClose = document.querySelector('.close-lines')
-let fullscreenNav = document.querySelector('.fullscreen-nav')
+$(function() {
+  let burgerBtn = $('.header__burger-btn')
+  let burgerBtnBlock = $('.burger-block')
+  let burgerBtnClose = $('.close-lines')
+  let fullscreenNav = $('.fullscreen-nav')
 
-burgerBtn.addEventListener('click', function showMenu () {
-  burgerBtnBlock.classList.toggle('visually-hidden')
-  burgerBtnClose.classList.toggle('visually-hidden')
+  burgerBtn.on('click', (e) => {
+    burgerBtnBlock.toggleClass('visually-hidden')
+    burgerBtnClose.toggleClass('visually-hidden')
 
-  let menuDisplay = getComputedStyle(fullscreenNav).display
-  if (menuDisplay === 'none') {
-    fullscreenNav.style.display = 'block'
-  }
-  else {
-    fullscreenNav.style.display = 'none'
-  }
+    fullscreenNav.fadeToggle(300)
+  })
 })
-
 
 // Для секции "команда"
-let teamItemAcc = $('.team__item')
+$(function() {
+  let teamItemAcc = $('.team__item')
 
-teamItemAcc.on('click', (e) => {
-  e.preventDefault()
+  teamItemAcc.on('click', (e) => {
+    e.preventDefault()
 
-  let elem = $(e.currentTarget)
+    let elem = $(e.currentTarget)
 
-  teamItemAcc.removeClass('team__item--active')
-  elem.addClass('team__item--active')
+    if (elem.hasClass('team__item--active')) {
+      elem.removeClass('team__item--active')
+    }
+    else {
+      teamItemAcc.removeClass('team__item--active')
+      elem.addClass('team__item--active')
+    }
+  })
 })
 
-// Для секции "меню"
-let menuItemAcc = $('.menu__item')
+  // Для секции "меню"
+$(function() {
+  let menuItemAcc = $('.menu__item')
+  let menuClose = $('.menu-content__close')
 
-menuItemAcc.on('click', (e) => {
-  e.preventDefault()
+  menuItemAcc.on('click', (e) => {
+    e.preventDefault()
 
-  let elem = $(e.currentTarget)
+    let elem = $(e.currentTarget)
 
-  if (elem.hasClass('menu__item--active')) {
-    elem.removeClass('menu__item--active')
-  }
-  else {
-    menuItemAcc.removeClass('menu__item--active')
-    elem.addClass('menu__item--active')
-  }
+    if (elem.hasClass('menu__item--active')) {
+      elem.removeClass('menu__item--active')
+    }
+    else {
+      menuItemAcc.removeClass('menu__item--active')
+      elem.addClass('menu__item--active')
+    }
+  })
+
+  menuClose.on('click', (e) => {
+    let elem = $(e.currentTarget)
+
+    if (elem.hasClass('menu__item--active')) {
+      elem.removeClass('menu__item--active')
+    }
+  })
+
 })
 
 // Для секции "карта"
-  // Подключение карты
-ymaps.ready(init);
-let myMap;
+$(function() {
+    // Подключение карты
+  ymaps.ready(init);
+  let myMap;
+  let mapZoom;
 
-function init() {
-  myMap = new ymaps.Map("map", {
-      center: [59.91817154482064,30.30557799999997],
-      zoom: 11,
-      controls: [],
-      behaviors: ['ruler', 'scrollZoom'],
-  });
-  myMap.behaviors.disable('ruler');
-  myMap.behaviors.disable('scrollZoom');
-
-  // Коллекция меток
-  let coords = [
-      [59.94554327989287,30.38935262114668],
-      [59.91142323563909,30.50024587065841],
-      [59.88693161784606,30.319658102103713],
-      [59.97033574821672,30.315194906302924]
-  ],
-  myCollection = new ymaps.GeoObjectCollection({}, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/content/map-marker.png',
-        iconImageSize: [46, 57],
-        iconImageOffset: [-26, -52]
-    });
-
-  for (var i = 0; i < coords.length; i++) {
-      myCollection.add(new ymaps.Placemark(coords[i]));
+  if (document.documentElement.clientWidth > 768) {
+    mapZoom = 11
+  }
+  else {
+    mapZoom = 10.4
   }
 
-  myMap.geoObjects.add(myCollection);
+  function init() {
+    myMap = new ymaps.Map("map", {
+        center: [59.91817154482064,30.30557799999997],
+        zoom: mapZoom,
+        controls: [],
+        behaviors: ['ruler', 'scrollZoom'],
+    });
+    myMap.behaviors.disable('ruler');
+    myMap.behaviors.disable('scrollZoom');
 
-}
+    // Коллекция меток
+    let coords = [
+        [59.94554327989287,30.38935262114668],
+        [59.91142323563909,30.50024587065841],
+        [59.88693161784606,30.319658102103713],
+        [59.97033574821672,30.315194906302924]
+    ],
+    myCollection = new ymaps.GeoObjectCollection({}, {
+          iconLayout: 'default#image',
+          iconImageHref: 'img/content/map-marker.png',
+          iconImageSize: [46, 57],
+          iconImageOffset: [-26, -52]
+      });
+
+    for (var i = 0; i < coords.length; i++) {
+        myCollection.add(new ymaps.Placemark(coords[i]));
+    }
+
+    myMap.geoObjects.add(myCollection);
+
+  }
+})
+
+//FancyBox
+
+//Слайдер секции "Бургер"
+$(function(){
+
+  let moveSlide = function (container, itemNum) {
+    let
+        items = container.find('.burgers__content'),
+        activeItem = items.filter('.burgers__content--active'),
+        reqItem = items.eq(itemNum),
+        reqIndex = reqItem.index(),
+        list = container.find('.burgers__list'),
+        duration = 500
+
+    if (reqItem.length) {
+      list.animate({
+        'left' : -reqIndex * 100 + '%'
+      }, duration, function () {
+        activeItem.removeClass('burgers__content--active');
+        reqItem.addClass('burgers__content--active');
+      })
+    }
+  }
+
+  $('.slider__section').on('click', (e) => {
+    e.preventDefault();
+
+    let $this = $(e.currentTarget),
+        container = $this.closest('.burgers'),
+        items = container.find('.burgers__content'),
+        activeItem = items.filter('.burgers__content--active'),
+        existedItem, edgeItem, reqItem;
+
+    if ($this.hasClass('slider__section--next')) {
+      existedItem = activeItem.next();
+      edgeItem = items.first();
+    }
+    if ($this.hasClass('slider__section--prev')) {
+      existedItem = activeItem.prev();
+      edgeItem = items.last();
+    }
+
+    reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
+    moveSlide(container, reqItem);
+  })
+
+})
+
+

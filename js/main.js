b662dabb254c6c94e0341f91fd8792b25f851c77
Mariya Molchanova <molchanova.m2903@gmail.com>
2017-11-24 00:30:05 +0700

    pagination work correctly

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
-    // responsiveHeight: 651,
+    menu: '#menu',
     responsiveWidth: 481,
     verticalCentered:false
   });
   $(document).on('click', '#moveTo', function(){
     $.fn.fullpage.moveTo('page7');
   });
-
 
 })
 

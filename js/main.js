605ca5ccafc5f0d9614479fe5fc84bda0f1b1df1
Mariya Molchanova <molchanova.m2903@gmail.com>
2017-11-26 23:45:11 +0700

    for review

--- js/main.js
+++ js/main.js
@@ -1,117 +1,118 @@
+'use strict';
+
 //OPS
-$(document).ready(function() {
+$(document).ready(function () {
   $('#fullpage').fullpage({
     anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
     menu: '#menu',
     responsiveWidth: 769,
-    verticalCentered:false
+    verticalCentered: false
   });
-  $(document).on('click', '#moveToGeneral', function(){
+  $(document).on('click', '#moveToGeneral', function () {
     $.fn.fullpage.moveTo('page7');
   });
-  $(document).on('click', '#moveTo1', function(){
+  $(document).on('click', '#moveTo1', function () {
     $.fn.fullpage.moveTo('page7');
   });
-  $(document).on('click', '#moveTo2', function(){
+  $(document).on('click', '#moveTo2', function () {
     $.fn.fullpage.moveTo('page7');
   });
-  $(document).on('click', '#moveTo3', function(){
+  $(document).on('click', '#moveTo3', function () {
     $.fn.fullpage.moveTo('page7');
   });
-  $(document).on('click', '#moveTo4', function(){
+  $(document).on('click', '#moveTo4', function () {
     $.fn.fullpage.moveTo('page7');
   });
-  $(document).on('click', '#moveTo5', function(){
+  $(document).on('click', '#moveTo5', function () {
     $.fn.fullpage.moveTo('page7');
   });
-})
+});
 
 // Для полноэкранного меню
-$(function() {
-  let hamburgerBtn = $('.hamburger')
-  let hamburgerBtnBlock = $('.hamburger-block')
-  let hamburgerBtnClose = $('.hamburger__close-lines')
-  let fullscreenNav = $('.header__nav')
-  let sectionHeight = $('.section').css('height')
+$(function () {
+  var hamburgerBtn = $('.hamburger');
+  var hamburgerBtnBlock = $('.hamburger-block');
+  var hamburgerBtnClose = $('.hamburger__close-lines');
+  var fullscreenNav = $('.header__nav');
+  var sectionHeight = $('.section').css('height');
   function disable() {
-    fullscreenNav.animate({opacity:0}, 200, function() {
-      $(this).css('height', 0).removeClass('header__nav--active')
-    })
-    hamburgerBtnBlock.removeClass('visually-hidden')
-    hamburgerBtnClose.addClass('visually-hidden')
-    $('body').removeClass('ov-hidden')
- }
+    fullscreenNav.animate({ opacity: 0 }, 200, function () {
+      $(this).css('height', 0).removeClass('header__nav--active');
+    });
+    hamburgerBtnBlock.removeClass('visually-hidden');
+    hamburgerBtnClose.addClass('visually-hidden');
+    $('body').removeClass('ov-hidden');
+  }
 
-  hamburgerBtn.on('click', (e) => {
+  hamburgerBtn.on('click', function (e) {
     if (!fullscreenNav.hasClass('header__nav--active')) {
-      fullscreenNav.addClass('header__nav--active')
-      fullscreenNav.css('height', sectionHeight)
-      fullscreenNav.animate({opacity:1}, 100)
-      hamburgerBtnBlock.addClass('visually-hidden')
-      hamburgerBtnClose.removeClass('visually-hidden')
-      $('body').addClass('ov-hidden')
+      fullscreenNav.addClass('header__nav--active');
+      fullscreenNav.css('height', sectionHeight);
+      fullscreenNav.animate({ opacity: 1 }, 100);
+      hamburgerBtnBlock.addClass('visually-hidden');
+      hamburgerBtnClose.removeClass('visually-hidden');
+      $('body').addClass('ov-hidden');
 
       // $(document).on('wheel', (e) => {
       //   e.preventDefault()
       //     console.log('wheeeel')
       // })
-
     } else {
-      disable()
+      disable();
     }
-    $('.nav__item').on('click', (e) => {
-      disable()
-    })
-  })
-})
+    $('.nav__item').on('click', function (e) {
+      disable();
+    });
+  });
+});
 
 // Для секции "Бургер"
-  //Состав
-$(function() {
-  let composition = $('.burgers__composition')
+//Состав
+$(function () {
+  var composition = $('.burgers__composition');
 
-  $('.burgers__container').on('click', (e) => {
+  $('.burgers__container').on('click', function (e) {
 
-    let elem = $(e.target).closest(composition)
+    var elem = $(e.target).closest(composition);
     if (elem.length) {
-      elem.toggleClass('burgers__composition--active')
+      elem.toggleClass('burgers__composition--active');
     } else {
-      composition.removeClass('burgers__composition--active')
+      composition.removeClass('burgers__composition--active');
     }
-  })
+  });
+});
 
-})
+//Слайдер секции
+$(function () {
 
-  //Слайдер секции
-$(function(){
-
-  let moveSlide = function (container, itemNum) {
-    let
-        items = container.find('.burgers__content'),
+  var moveSlide = function moveSlide(container, itemNum) {
+    var items = container.find('.burgers__content'),
         activeItem = items.filter('.burgers__content--active'),
         reqItem = items.eq(itemNum),
         reqIndex = reqItem.index(),
         list = container.find('.burgers__list'),
-        duration = 500
+        duration = 500;
 
     if (reqItem.length) {
       list.animate({
-        'left' : -reqIndex * 100 + '%'
+        'left': -reqIndex * 100 + '%'
       }, duration, function () {
         activeItem.removeClass('burgers__content--active');
         reqItem.addClass('burgers__content--active');
-      })
+      });
     }
-  }
+  };
 
-  $('.slider-arrow__btn').on('click', (e) => {
+  $('.slider-arrow__btn').on('click', function (e) {
     e.preventDefault();
 
-    let $this = $(e.currentTarget),
+    var $this = $(e.currentTarget),
         container = $this.closest('.burgers'),
         items = container.find('.burgers__content'),
         activeItem = items.filter('.burgers__content--active'),
-        existedItem, edgeItem, reqItem;
+        existedItem = void 0,
+        edgeItem = void 0,
+        reqItem = void 0;
 
     if ($this.hasClass('slider-arrow__btn--next')) {
       existedItem = activeItem.next();
@@ -124,165 +125,149 @@ $(function(){
 
     reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
     moveSlide(container, reqItem);
-  })
-
-})
+  });
+});
 
 // Для секции "команда"
-$(function() {
-  let teamItemAcc = $('.team__item')
+$(function () {
+  var teamItemAcc = $('.team__item');
 
-  teamItemAcc.on('click', (e) => {
-    e.preventDefault()
+  teamItemAcc.on('click', function (e) {
+    e.preventDefault();
 
-    let elem = $(e.currentTarget)
+    var elem = $(e.currentTarget);
 
     if (elem.hasClass('team__item--active')) {
-      elem.removeClass('team__item--active')
+      elem.removeClass('team__item--active');
+    } else {
+      teamItemAcc.removeClass('team__item--active');
+      elem.addClass('team__item--active');
     }
-    else {
-      teamItemAcc.removeClass('team__item--active')
-      elem.addClass('team__item--active')
-    }
-  })
-})
+  });
+});
 
 // Для секции "меню"
-$(function() {
-  let menuItemAcc = $('.menu__item')
-  let menuClose = $('.menu-content__close')
+$(function () {
+  var menuItemAcc = $('.menu__item');
+  var menuClose = $('.menu-content__close');
 
-  $('.menu').on('click', (e) => {
-    e.preventDefault()
+  $('.menu').on('click', function (e) {
+    e.preventDefault();
 
-  if (document.documentElement.clientWidth < 481) {
-    $('.hamburger').fadeToggle(300)
-  }
+    if (document.documentElement.clientWidth < 481) {
+      $('.hamburger').fadeToggle(300);
+    }
 
-    let elem = $(e.target).closest(menuItemAcc)
+    var elem = $(e.target).closest(menuItemAcc);
 
     if (elem.length) {
 
-      elem.siblings().removeClass('menu__item--active')
-      elem.toggleClass('menu__item--active')
+      elem.siblings().removeClass('menu__item--active');
+      elem.toggleClass('menu__item--active');
     } else {
-      menuItemAcc.removeClass('menu__item--active')
+      menuItemAcc.removeClass('menu__item--active');
     }
+  });
 
-  })
-
-  menuClose.on('click', (e) => {
-    let elem = $(e.currentTarget)
+  menuClose.on('click', function (e) {
+    var elem = $(e.currentTarget);
 
     if (elem.hasClass('menu__item--active')) {
-      elem.removeClass('menu__item--active')
+      elem.removeClass('menu__item--active');
     }
-  })
-
-})
-
+  });
+});
 
 // Для секции "Комментарии"
-$(function() {
-  let comment = $('.comments__img')
+$(function () {
+  var comment = $('.comments__img');
 
-  comment.hover(function(e) {
-    let elem = $(e.target).closest(comment)
-      elem.toggleClass('comments__img--active')
-  })
+  comment.hover(function (e) {
+    var elem = $(e.target).closest(comment);
+    elem.toggleClass('comments__img--active');
+  });
 
-  comment.on('click', (e) => {
-    let elem = $(e.target).closest(comment)
+  comment.on('click', function (e) {
+    var elem = $(e.target).closest(comment);
     if (elem.length) {
-      comment.removeClass('comments__img--active')
-      elem.toggleClass('comments__img--active')
+      comment.removeClass('comments__img--active');
+      elem.toggleClass('comments__img--active');
     }
-  })
-  $("a.gallery2").fancybox();  //FancyBox
-})
+  });
+  $("a.gallery2").fancybox(); //FancyBox
+});
 
 // Для секции "карта"
-$(function() {
-    // Подключение карты
+$(function () {
+  // Подключение карты
   ymaps.ready(init);
-  let myMap;
-  let mapZoom;
+  var myMap = void 0;
+  var mapZoom = void 0;
 
   if (document.documentElement.clientWidth > 768) {
-    mapZoom = 11
-  }
-  else {
-    mapZoom = 10
+    mapZoom = 11;
+  } else {
+    mapZoom = 10;
   }
 
   function init() {
     myMap = new ymaps.Map("map", {
-        center: [59.91817154482064,30.30557799999997],
-        zoom: mapZoom,
-        controls: [],
-        behaviors: ['ruler', 'scrollZoom'],
+      center: [59.91817154482064, 30.30557799999997],
+      zoom: mapZoom,
+      controls: [],
+      behaviors: ['ruler', 'scrollZoom']
     });
     myMap.behaviors.disable('ruler');
     myMap.behaviors.disable('scrollZoom');
 
     // Коллекция меток
-    let coords = [
-        [59.94554327989287,30.38935262114668],
-        [59.91142323563909,30.50024587065841],
-        [59.88693161784606,30.319658102103713],
-        [59.97033574821672,30.315194906302924]
-    ],
-    myCollection = new ymaps.GeoObjectCollection({}, {
-          iconLayout: 'default#image',
-          iconImageHref: 'img/content/map-marker.png',
-          iconImageSize: [46, 57],
-          iconImageOffset: [-26, -52]
-      });
+    var coords = [[59.94554327989287, 30.38935262114668], [59.91142323563909, 30.50024587065841], [59.88693161784606, 30.319658102103713], [59.97033574821672, 30.315194906302924]],
+        myCollection = new ymaps.GeoObjectCollection({}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/content/map-marker.png',
+      iconImageSize: [46, 57],
+      iconImageOffset: [-26, -52]
+    });
 
     for (var i = 0; i < coords.length; i++) {
-        myCollection.add(new ymaps.Placemark(coords[i]));
+      myCollection.add(new ymaps.Placemark(coords[i]));
     }
 
     myMap.geoObjects.add(myCollection);
-
   }
-})
-
+});
 
 //Для секции "Заказ"
-$(function() {
-  let submitForm = function (e) {
+$(function () {
+  var submitForm = function submitForm(e) {
     e.preventDefault();
 
-    let form = $(e.target),
+    var form = $(e.target),
         data = form.serialize(),
         url = form.attr('action');
 
-    let request = $.ajax({
+    var request = $.ajax({
       url: url,
       type: 'POST',
       data: data,
       dataType: 'JSON'
     });
 
-    request.done(function(msg) {
-      let mes = msg.mes,
+    request.done(function (msg) {
+      var mes = msg.mes,
           status = msg.status;
 
       if (status === 'ОК') {
-        form.append('<p class="success">' + mes +'</p>');
+        form.append('<p class="success">' + mes + '</p>');
       } else {
-        form.append('<p class="error">' + mes +'</p>');
+        form.append('<p class="error">' + mes + '</p>');
       }
     });
 
-    request.fail(function(jqXHR, textStatus) {
+    request.fail(function (jqXHR, textStatus) {
       alert("Request failed: " + textStatus);
     });
-
   };
 
-
   $('#order-form').on('submit', submitForm);
-
-})
+});
